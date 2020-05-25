071be4564ff58ea78b84c8a1454f95b352040fd2
LARABA MOHAMMED SEDDIK <mslaraba@gmail.com>
2020-05-25 08:29:12 +0100

    Aussie palette

---
+++ OptimIT.Palettes/AUPalette.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace OptimIT.Palettes
+{
+    public class AUPalette
+    {
+        static AUPalette _INST;
+        public static AUPalette INST => _INST ?? new AUPalette();
+        public SolidColorBrush BeeKeeper => "#f6e58d".ToBrush();
+        public SolidColorBrush SpicedNectarine => "#ffbe76".ToBrush();
+        public SolidColorBrush PinkGlamour => "#ffbe76".ToBrush();
+        public SolidColorBrush JuneBud => "#badc58".ToBrush();
+        public SolidColorBrush CoastalBreeze => "#dff9fb".ToBrush();
+
+
+        public SolidColorBrush Turbo => "#f9ca24".ToBrush();
+        public SolidColorBrush QuinceJelly => "#f0932b".ToBrush();
+        public SolidColorBrush CarminePink => "#eb4d4b".ToBrush();
+        public SolidColorBrush PureApple => "#6ab04c".ToBrush();
+        public SolidColorBrush HintOfIcePack => "#c7ecee".ToBrush();
+
+
+        public SolidColorBrush MiddleBlue => "#7ed6df".ToBrush();
+        public SolidColorBrush HelioTrope => "#e056fd".ToBrush();
+        public SolidColorBrush ExodusFruit => "#686de0".ToBrush();
+        public SolidColorBrush DeepKoamaru => "#30336b".ToBrush();
+        public SolidColorBrush SoaringEagle => "#95afc0".ToBrush();
+
+
+        public SolidColorBrush GreenlandGreen => "#22a6b3".ToBrush();
+        public SolidColorBrush SteelPink => "#be2edd".ToBrush();
+        public SolidColorBrush Blurple => "#4834d4".ToBrush();
+        public SolidColorBrush DeepCove => "#4834d4".ToBrush();
+        public SolidColorBrush WiearGrey => "#535c68".ToBrush();
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 ### Project
 Some C# color palettes, all in one place 😊. Starting by WPF C# & xaml , then .netstandard, ...
 
+### Created by
+LARABA MOHAMMED SEDDIK
+
+[OptimIT](https://optimit.dz "OptimIT"), Touggourt, Algeria ![DZ](https://www.countryflags.io/dz/shiny/24.png)
+
 ### Demo
 
 ![Demo app](https://raw.githubusercontent.com/seddik/optimit.palettes/master/img/main.png)
@@ -11,8 +16,6 @@ Some C# color palettes, all in one place 😊. Starting by WPF C# & xaml , then 
 
 ![Demo app](https://raw.githubusercontent.com/seddik/optimit.palettes/master/img/gb.png)
 
+![Demo app](https://raw.githubusercontent.com/seddik/optimit.palettes/master/img/au.png)
 
-### Created by
-LARABA MOHAMMED SEDDIK
 
-[OptimIT](https://optimit.dz "OptimIT"), Touggourt, Algeria ![DZ](https://www.countryflags.io/dz/shiny/24.png)
